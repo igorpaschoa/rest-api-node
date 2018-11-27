83d28be72cf1eb57d31b29a9d04ad7b66dbc0c0d
Igor Paschoa <igor_paschoa@hotmail.com>
2018-11-27 21:36:19 -0200

    adding error page

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
-let express = require("express");
+let express = require('express');
 let app = express();
 
-let personRoute = require("./routes/person");
+let personRoute = require('./routes/person');
+
+let path = require('path');
 
 app.use((req, res, next) => {
   console.log(`${new Date().toString()} => ${req.originalUrl}`);
-  next;
+  next();
 });
 
 app.use(personRoute);
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 app.use((req, res, next) => {
-  res.status(404).send("Error 404. Page not found. :)");
+  res.status(404).send('Error 404. Page not found. :)');
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.sendFile(path.join(__dirname, '../public/500.html'));
 });
 
 const PORT = process.env.PORT || 5000;
--- src/routes/person.js
+++ src/routes/person.js
@@ -1,16 +1,20 @@
-let express = require("express");
+let express = require('express');
 let router = express.Router();
 
-router.get("/person", (req, res) => {
+router.get('/person', (req, res) => {
   if (req.query.name) {
     res.send(`The name of the person is ${req.query.name}`);
   } else {
-    res.send("You have requested a person");
+    res.send('You have requested a person');
   }
 });
 
-router.get("/person/:name", (req, res) => {
+router.get('/person/:name', (req, res) => {
   res.send(`The name of the person is ${req.params.name}`);
+});
+
+router.get('/error', (req, res) => {
+  throw new Error('This is a forced error');
 });
 
 module.exports = router;
