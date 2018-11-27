622f34345b00f088b150bcf16980e18ea5278f77
Igor Paschoa <igor_paschoa@hotmail.com>
2018-11-27 21:18:06 -0200

    added more routes and a 404 error handler

--- src/index.js
+++ src/index.js
@@ -3,8 +3,17 @@ let app = express();
 
 let personRoute = require("./routes/person");
 
+app.use((req, res, next) => {
+  console.log(`${new Date().toString()} => ${req.originalUrl}`);
+  next;
+});
+
 app.use(personRoute);
 app.use(express.static("public"));
+
+app.use((req, res, next) => {
+  res.status(404).send("Error 404. Page not found. :)");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.info(`Server has started on ${PORT}`));
--- src/routes/person.js
+++ src/routes/person.js
@@ -2,7 +2,15 @@ let express = require("express");
 let router = express.Router();
 
 router.get("/person", (req, res) => {
-  res.send("You hve requested a person");
+  if (req.query.name) {
+    res.send(`The name of the person is ${req.query.name}`);
+  } else {
+    res.send("You have requested a person");
+  }
+});
+
+router.get("/person/:name", (req, res) => {
+  res.send(`The name of the person is ${req.params.name}`);
 });
 
 module.exports = router;
